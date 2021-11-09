4d7e1e951ae63c73baa10f2961324cb9bc86acc9
Arairn <Arairn@gmail.com>
2021-11-09 17:03:45 +0300

    4(15)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    Vector2 moveDirection;
     Rigidbody2D rb;
+    Animator playerAnimator;
 
 
     private void Start()
     {
+        DontDestroyOnLoad(gameObject);
         rb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))*moveSpeed;
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized*moveSpeed;
+        if (rb.velocity.SqrMagnitude() >= 0.01f)
+        {
+            playerAnimator.SetBool("isMoving", true);
+            playerAnimator.SetFloat("lastMoveX", rb.velocity.x);
+            playerAnimator.SetFloat("lastMoveY", rb.velocity.y);
+        }
+        else
+        {
+            playerAnimator.SetBool("isMoving", false);
+        }
+
+        playerAnimator.SetFloat("moveX", rb.velocity.x);
+        playerAnimator.SetFloat("moveY", rb.velocity.y);
+        
+        
     }
 }
