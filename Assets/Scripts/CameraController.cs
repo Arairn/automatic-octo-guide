e239e0c01f590f897d22219f39d72d8fd94334a4
Arairn <Arairn@gmail.com>
2021-11-13 02:25:12 +0300

    5(25)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,27 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+
 
 public class CameraController : MonoBehaviour
 {
     public Transform target;
     public float cameraSpeed;
-    Vector3 targetCamPosition;
+    Vector3 targetCamPosition, currentPosition;
+    public Tilemap map;
+    float x, y, halfheight, halfWidth;
+    Vector3 bottomLeftLimit, topRightLimit;
+
+
     // Start is called before the first frame update
     void Start()
     {
         SetTarget(PlayerController.instance.transform);
+        map = GameObject.FindWithTag("map").GetComponent<Tilemap>();
+
+        bottomLeftLimit = map.localBounds.min;
+        topRightLimit = map.localBounds.max;
+        halfheight = Camera.main.orthographicSize;
+        halfWidth = halfheight * Camera.main.aspect;
+
     }
 
     public void SetTarget(Transform newTarget)
     {
         target = newTarget;
     }
-    4
+    
     // Update is called once per frame
-    void LateUpdate()
+    void FixedUpdate()
     {
         targetCamPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
-        transform.position = Vector3.Lerp(transform.position, targetCamPosition, Time.deltaTime*cameraSpeed);
+        currentPosition = transform.position;
+
+        transform.position = Vector3.Lerp(currentPosition, targetCamPosition, Time.deltaTime*cameraSpeed);
+        x = Mathf.Clamp(transform.position.x, bottomLeftLimit.x+halfWidth, topRightLimit.x-halfWidth);
+        y = Mathf.Clamp(transform.position.y, bottomLeftLimit.y+halfheight, topRightLimit.y-halfheight);
+
+        transform.position = new Vector3(x, y, transform.position.z);
     }
 }
